4158a0c75e8c8459dcfce64706cfe25f36b95c85
RAJA AHMED <ahmedrajay@gmail.com>
2022-02-01 12:53:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function calculate() {
     .then(res => res.json())
     .then(data => {
       //  console.log(data);
-      const rate = data.rates[currency_two] / data.rates[currency_one];
+      const rate = data.conversion_rates[currency_two] / data.conversion_rates[currency_one];
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
       amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
     });
